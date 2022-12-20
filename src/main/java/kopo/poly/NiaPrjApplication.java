c4f7096786aad640465de0180ba94df37e4ee515
beomjung <k980220@naver.com>
2022-12-20 23:40:36 +0900

    SpringBoot main 파일

--- src/main/java/kopo/poly/NiaPrjApplication.java
+++ src/main/java/kopo/poly/NiaPrjApplication.java
@@ -2,6 +2,8 @@ package kopo.poly;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class NiaPrjApplication {
@@ -9,5 +11,8 @@ public class NiaPrjApplication {
     public static void main(String[] args) {
         SpringApplication.run(NiaPrjApplication.class, args);
     }
+
+    @Bean
+    public RestTemplate restTemplate(){return new RestTemplate();}
 
 }
